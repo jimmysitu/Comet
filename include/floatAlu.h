94c285ab291353bf3f187978ca1e7300fc609362
Lauric Desauw <ldesauw@deutzia.irisa.fr>
2019-05-29 15:17:57 +0200

    CMP instructions

--- include/floatAlu.h
+++ include/floatAlu.h
@@ -22,9 +22,8 @@ private :
 	int state = 0;
 public :
 	void process(struct DCtoEx dctoEx, struct ExtoMem &extoMem, bool &stall)
-{
-			
-    	  ac_int<1, false> f1Sign;
+{			
+   	  ac_int<1, false> f1Sign;
 	  ac_int<1, false> f2Sign;
  
 	  ac_int<24, false> f1Mantissa;
@@ -142,8 +141,13 @@ public :
 
 					f1Exp = dctoEx.rhs.slc<8>(23) - 127;
 					f2Exp = dctoEx.lhs.slc<8>(23) - 127;
-						if (dctoEx.funct3)
-						{
+					
+					f1Sign = dctoEx.rhs.slc<1>(31);
+					f2Sign = dctoEx.lhs.slc<1>(31);
+					if(dctoEx.funct3) //FMAX
+					{
+						if(f1Exp == f2Sign)
+						{if(f1Sign){  // both are positive
 							if (f1Exp > f2Exp)
 								extoMem.result = dctoEx.rhs;
 							else
@@ -157,10 +161,8 @@ public :
 									else
 										extoMem.result = dctoEx.lhs;
 								}
-							}
-						}        
-						else
-						{
+							}}
+						else{ // both are negative
 							if (f1Exp < f2Exp)
 								extoMem.result = dctoEx.rhs;
 							else
@@ -174,14 +176,164 @@ public :
 									else
 										extoMem.result = dctoEx.lhs;
 								}
-							}
-						}                             
+							}}
+						} //End if(f1Exp == f2Sign)
+						else // rhs and lhs have different sign                            	
+						{
+
+						if(f1Sign < f2Sign) // rhs positive and lhs negative 
+							extoMem.result = dctoEx.lhs;
+						else // rhs negative and lhs positive
+							extoMem.result = dctoEx.rhs;
+						}  
+					}
+   
+					else //FMIN
+					{
+						if(f1Exp == f2Sign)
+						{if(f1Sign){  // both are positive
+							if (f1Exp < f2Exp)
+								extoMem.result = dctoEx.rhs;
+							else
+							{
+								if(f2Exp < f1Exp)
+									extoMem.result = dctoEx.lhs;
+								else
+								{	
+									if(f1Mantissa < f2Mantissa)
+										extoMem.result = dctoEx.rhs;
+									else
+										extoMem.result = dctoEx.lhs;
+								}
+							}}
+						else{ // both are negative
+							if (f1Exp > f2Exp)
+								extoMem.result = dctoEx.rhs;
+							else
+							{
+								if(f2Exp > f1Exp)
+									extoMem.result = dctoEx.lhs;
+								else
+								{	
+									if(f1Mantissa > f2Mantissa)
+										extoMem.result = dctoEx.rhs;
+									else
+										extoMem.result = dctoEx.lhs;
+								}
+							}}
+						} //End if(f1Exp == f2Sign)
+						else // rhs and lhs have different sign                            	
+						{
+
+						if(f1Sign < f2Sign) // rhs positive and lhs negative 
+							extoMem.result = dctoEx.lhs;
+						else // rhs negative and lhs positive
+							extoMem.result = dctoEx.rhs;
+						}  
+					}
+
+					
                           break;                                                  
                                                                                   
                   case RISCV_FLOAT_OP_CVTWS :                                     
                           break;                                                  
                                                                                   
-                  case RISCV_FLOAT_OP_CMP  :                                      
+                  case RISCV_FLOAT_OP_CMP  :
+					f1Mantissa = dctoEx.rhs.slc<23>(0);
+					f2Mantissa = dctoEx.lhs.slc<23>(0);
+					f1Mantissa[23] = 1;
+					f2Mantissa[23] = 1;
+
+					f1Exp = dctoEx.rhs.slc<8>(23) - 127;
+					f2Exp = dctoEx.lhs.slc<8>(23) - 127;
+					
+					f1Sign = dctoEx.rhs.slc<1>(31);
+					f2Sign = dctoEx.lhs.slc<1>(31);
+						  
+					switch(dctoEx.funct3)
+							{
+								case 0:
+		
+									if(f1Sign == f2Sign)
+									{
+										if(f1Sign)
+										{
+											if (f1Exp >= f2Exp)
+												extoMem.result[0] = true;
+											else
+											{
+												if(f2Exp == f1Exp)
+												{	
+												if(f1Mantissa >= f2Mantissa)
+													extoMem.result[0] = true;
+												}
+											}	
+										}
+										else
+										{
+											if (f1Exp <= f2Exp)
+												extoMem.result[0] = true;
+											else
+											{
+											if(f2Exp == f1Exp)
+												{	
+												if(f1Mantissa <= f2Mantissa)
+													extoMem.result[0] = true;
+												}
+											}
+										}
+									}
+									else
+									{
+										if(f1Sign > f2Sign)
+											extoMem.result[0] = true ;
+									}
+
+								break;
+								
+								case 1:
+										
+									if(f1Sign == f2Sign)
+									{
+										if(f1Sign)
+										{
+											if (f1Exp > f2Exp)
+												extoMem.result[0] = true;
+											else
+											{
+												if(f2Exp == f1Exp)
+												{	
+												if(f1Mantissa > f2Mantissa)
+													extoMem.result[0] = true;
+												}
+											}	
+										}
+										else
+										{
+											if (f1Exp < f2Exp)
+												extoMem.result[0] = true;
+											else
+											{
+											if(f2Exp == f1Exp)
+												{	
+												if(f1Mantissa < f2Mantissa)
+													extoMem.result[0] = true;
+												}
+											}
+										}
+									}
+									else
+									{
+										if(f1Sign > f2Sign)
+											extoMem.result[0] = true ;
+									}
+
+								break;
+
+								case 2:
+									extoMem.result[0] = dctoEx.rhs = dctoEx.lhs;
+								break;
+							}                                    
                           break;                                                  
                                                                                   
                   case RISCV_FLOAT_OP_CVTSW :                                     
