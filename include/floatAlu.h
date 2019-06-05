27b4cd915bea1d02499d55d43216484906b540a0
Lauric Desauw <ldesauw@deutzia.irisa.fr>
2019-06-05 16:51:04 +0200

    Compile with Catapult (SQRT still missing)

--- include/floatAlu.h
+++ include/floatAlu.h
@@ -23,6 +23,7 @@ private :
 	ac_int<48,false> remainder = 0;
 	ac_int<48,false> f1Val = 0;
 	ac_int<48,false> f2Val = 0;
+	ac_int<24,false> tmp = 0;
 	
 public :
 	void process(struct DCtoEx dctoEx, struct ExtoMem &extoMem, bool &stall)
@@ -40,8 +41,10 @@ public :
 	  ac_int<48, false> outputMantissa;
 	  ac_int<23, false> resultMantissa;
  	  ac_int<9, false> outputExp; 
+
       float f1;
-	  int* p;
+	  int g;
+	  
 
 
       stall =false;       
@@ -296,18 +299,18 @@ public :
 					
                           break;                                                  
                                                                                   
-                  case RISCV_FLOAT_OP_CVTWS :   
+                  case RISCV_FLOAT_OP_CVTWS :
+					state = ((int) f1Exp) - 23;
+					stall = true; 
 					if(!dctoEx.rs2)
 						{
-							extoMem.result = (int) (( pow(2, ((int) f1Exp) -23) ) * (int) f1Mantissa);
-							if(f1Exp)
-								extoMem.result = - extoMem.result;
+							
+							extoMem.result =  (int) f1Mantissa;
 						}
 					else
 						{
-							extoMem.result = (unsigned int) (( pow(2, ((int) f1Exp) -23) ) * (int) f1Mantissa);
-						}
-				                                
+							extoMem.result =(unsigned int) f1Mantissa;
+						}                   
                           break;                                                  
                                                                                   
                   case RISCV_FLOAT_OP_CMP  :
@@ -397,11 +400,38 @@ public :
 							}                                    
                           break;                                                  
                                                                                   
-                  case RISCV_FLOAT_OP_CVTSW :     // Same thing if the interger is signed or not
-								f1 = (float) dctoEx.rhs;
-								p = (int*) &f1;
-								extoMem.result = (ac_int<32,false>) *p;
-
+                  case RISCV_FLOAT_OP_CVTSW :     
+								if(dctoEx.rs2) // FCVT.WU.S
+								{
+									for(int i = 31; i >= 0; i--)
+										if (dctoEx.rhs[i])
+											{
+												extoMem.result.set_slc(23, (ac_int<8,false>) (127 + i));
+												extoMem.result.set_slc(0, (dctoEx.rhs << (31 - i)).slc<23>(8) );
+											}
+								}
+								else // FCVT.W.S
+								{
+									if(dctoEx.rhs[31]) // the number is negative -> 2's complement
+									{
+										for(int i = 30; i >= 0; i--)
+											if (!dctoEx.rhs[i])
+												{
+													extoMem.result.set_slc(23, (ac_int<8,false>) (127 + i-1));
+													extoMem.result.set_slc(0, (dctoEx.rhs << (31 - i+1)).slc<23>(8) );
+												}
+									
+									}
+									else 
+									{
+										for(int i = 31; i >= 0; i--)
+											if (dctoEx.rhs[i])
+												{
+													extoMem.result.set_slc(23, (ac_int<8,false>) (127 + i));
+													extoMem.result.set_slc(0, (dctoEx.rhs << (31 - i)).slc<23>(8) );
+												}
+									}
+								}
                           break;                                                  
                                                                                   
                   case RISCV_FLOAT_OP_MVWX :                                      
@@ -428,54 +458,61 @@ public :
 		break;
           } 
   }            
-  else // state != 0 -> division loop
-   { // we unroll the loop 4 times (maybe there is a cleaner way to do it)
-    state--;
-	remainder = remainder << 1;
-	remainder[0] = f1Val[state];
-	if(remainder >= f2Val)
-	{
-		remainder = remainder - f2Val;
-		quotient[state] = 1;
-	}
+  else // state != 0 ->  loop
+   { 
+   switch(dctoEx.funct7)
+   {
+ 	  case RISCV_FLOAT_OP_DIV : 
+    	for(int i =0; i <4; i++) 
+    	{
+    		state--;
+			remainder = remainder << 1;
+			remainder[0] = f1Val[state];
+			if(remainder >= f2Val)
+			{
+				remainder = remainder - f2Val;
+				quotient[state] = 1;
+			}
+		}
+		if(!state)
+		{
+			outputSign = f1Sign ^ f2Sign;
+		  	outputExp = f1Exp - f2Exp + 127;
+			stall = false;
+  			extoMem.result.set_slc(0, quotient.slc<23>(1)); 
+  			extoMem.result.set_slc(23, outputExp.slc<8>(0));
+  			extoMem.result.set_slc(31, outputSign);
+	
+		}
+		break;
+		
+		case RISCV_FLOAT_OP_CVTWS : 
+			if (state > 0)
+			{
+				if (state >= 32)
+					{extoMem.result = extoMem.result << 32; state -= 32;}
+				else
+					{extoMem.result = extoMem.result << (state % 32); state = 0;}
+			}
+			else
+			{
+				if (state <= -32)
+					{extoMem.result = extoMem.result >> 32; state += 32;}
+				else
+					{extoMem.result = extoMem.result >> (32 - (state % 32)); state = 0;}
+			
+			}
+			
+			if (!state)
+			{
+				stall = false;
+				if(!dctoEx.rs2 && f1Exp)
+					extoMem.result = - extoMem.result; 
+			}
+			break;
+			
 	
 	
-	    state--;
-	remainder = remainder << 1;
-	remainder[0] = f1Val[state];
-	if(remainder >= f2Val)
-	{
-		remainder = remainder - f2Val;
-		quotient[state] = 1;
-	}
-	
-	    state--;
-	remainder = remainder << 1;
-	remainder[0] = f1Val[state];
-	if(remainder >= f2Val)
-	{
-		remainder = remainder - f2Val;
-		quotient[state] = 1;
-	}
-	
-	    state--;
-	remainder = remainder << 1;
-	remainder[0] = f1Val[state];
-	if(remainder >= f2Val)
-	{
-		remainder = remainder - f2Val;
-		quotient[state] = 1;
-	}
-	
-	if(!state)
-	{
-		outputSign = f1Sign ^ f2Sign;
-	  	outputExp = f1Exp - f2Exp + 127;
-		stall = false;
-  		extoMem.result.set_slc(0, quotient.slc<23>(1)); 
-  		extoMem.result.set_slc(23, outputExp.slc<8>(0));
-  		extoMem.result.set_slc(31, outputSign);
-
 	}
    }                                                        
   }  
