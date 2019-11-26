153d8ece57f4382e0afbb8f9c828d6b1c175e6e7
PATUREL Joseph <joseph.paturel@inria.fr>
2019-11-26 12:07:41 +0100

    Revert "Revert "Adds expect ALU (does not compile yet)""
    This reverts commit 9772473af8fcee1eb917f51ffd342a960131b7be

--- src/core.cpp
+++ src/core.cpp
@@ -454,7 +454,7 @@ void doCycle(struct Core &core, 		 //Core containing all values
 
 
     //declare temporary register file
-    ac_int<32, false> nextInst, multResult = 0;
+    ac_int<32, false> nextInst, multResult = 0, expectResult = 0;
 
     if (!localStall && !core.stallDm)
     	core.im->process(core.pc, WORD, LOAD, 0, nextInst, core.stallIm);
@@ -463,8 +463,14 @@ void doCycle(struct Core &core, 		 //Core containing all values
     decode(core.ftoDC, dctoEx_temp, core.regFile);
     core.basicALU.process(core.dctoEx, extoMem_temp, core.stallAlu);	//calling ALU: execute stage
     bool multUsed = core.multALU.process(core.dctoEx, multResult, core.stallAlu);	//calling ALU: execute stage
+    bool expectUsed = core.expectALU.process(core.dctoEx, expectResult, core.stallAlu);	//calling expectALU: execute stage
+
     if (multUsed)
         extoMem_temp.result = multResult;
+
+    if (expectUsed)
+        extoMem_temp.result = expectResult;
+
 
 
     memory(core.extoMem, memtoWB_temp);
