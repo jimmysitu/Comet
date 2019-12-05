f089d625dca277bce95ff9592ba829e9df88d60a
Joseph Paturel <joseph.paturel@inria.fr>
2019-12-05 16:37:34 +0100

    Add memory interface to the fetch stage

--- src/core.cpp
+++ src/core.cpp
@@ -7,20 +7,30 @@
 #endif  // __HLS__
 
 
-void fetch(ac_int<32, false> pc,
-           struct FtoDC &ftoDC,
-           ac_int<32, false> instruction)
+ac_int<32, false> valueRead;
+
+struct FtoDC fetch(ac_int<32, false> pc, static IncompleteMemory imInterface)
 {
-    ftoDC.instruction = instruction;
-    ftoDC.pc = pc;
-    ftoDC.nextPCFetch = pc + 4;
-    ftoDC.we = 1;
+  struct FtoDC ftoDC = FtoDC();
+
+  //get next instruction from memory
+  ac_int<32, false> nextInst;
+
+  imInterface.process(pc_in, WORD, LOAD, 0, nextInst, stallIm);
+
+  ftoDC.instruction = nextInst;
+  ftoDC.pc = pc;
+  ftoDC.nextPCFetch = pc + 4;
+  ftoDC.we = 1;
+
+  return ftoDC;
 }
 
-void decode(struct FtoDC ftoDC,
-            struct DCtoEx &dctoEx,
+struct DCtoEx decode(struct FtoDC ftoDC,
             ac_int<32, true> registerFile[32])
 {
+
+  struct DCtoEx dctoEx = DCtoEx();
     ac_int<32, false> pc = ftoDC.pc;
     ac_int<32, false> instruction = ftoDC.instruction;
 
@@ -200,13 +210,12 @@ void decode(struct FtoDC ftoDC,
     	dctoEx.useRs2 = 0;
     	dctoEx.useRs3 = 0;
     }
-
+    return dctoEx;
 }
 
-void memory(struct ExtoMem extoMem,
-            struct MemtoWB &memtoWB)
+struct MemtoWB memory(struct ExtoMem extoMem)
 {
-
+  struct MemtoWB memtoWB = MemtoWB();
     ac_int<2, false> datasize = extoMem.funct3.slc<2>(0);
     bool signenable = !extoMem.funct3.slc<1>(2);
     memtoWB.we = extoMem.we;
@@ -236,18 +245,20 @@ void memory(struct ExtoMem extoMem,
 
         break;
     }
+    return memtoWB;
 }
 
 
-void writeback(struct MemtoWB memtoWB,
-				struct WBOut &wbOut)
+struct WBOut writeback(struct MemtoWB memtoWB)
 {
+  struct WBOut wbOut = WBOut();
 	wbOut.we = memtoWB.we;
     if((memtoWB.rd != 0) && (memtoWB.we) && memtoWB.useRd){
       wbOut.rd = memtoWB.rd;
       wbOut.value = memtoWB.result;
     	wbOut.useRd = 1;
     }
+    return wbOut;
 }
 
 void branchUnit(ac_int<32, false> nextPC_fetch,
@@ -352,212 +363,165 @@ void forwardUnit(
 	}
 }
 
-/****************************************************************
- *  Copy functions
- ****************************************************************
-
-void copyFtoDC(struct FtoDC &dest, struct FtoDC src){
-    dest.pc = src.pc;
-    dest.instruction = src.instruction;
-    dest.nextPCFetch = src.nextPCFetch;
-    dest.we = src.we;
-}
-
-void copyDCtoEx(struct DCtoEx &dest, struct DCtoEx src){
-    dest.pc = src.pc;       // used for branch
-    dest.instruction = src.instruction;
-
-    dest.opCode = src.opCode;    // opCode = instruction[6:0]
-    dest.funct7 = src.funct7;    // funct7 = instruction[31:25]
-    dest.funct3 = src.funct3;    // funct3 = instruction[14:12]
-
-    dest.lhs = src.lhs;   //  left hand side : operand 1
-    dest.rhs = src.rhs;   // right hand side : operand 2
-    dest.datac = src.datac; // ST, BR, JAL/R,
-
-    //For branch unit
-    dest.nextPCDC = src.nextPCDC;
-    dest.isBranch = src.isBranch;
-
-    //Information for forward/stall unit
-    dest.useRs1 = src.useRs1;
-    dest.useRs2 = src.useRs2;
-    dest.useRs3 = src.useRs3;
-    dest.useRd = src.useRd;
-    dest.rs1 = src.rs1;       // rs1    = instruction[19:15]
-    dest.rs2 = src.rs2;       // rs2    = instruction[24:20]
-    dest.rs3 = src.rs3;
-    dest.rd = src.rd;        // rd     = instruction[11:7]
-
-    //Register for all stages
-    dest.we = src.we;
-}
-
-void copyExtoMem(struct ExtoMem &dest, struct ExtoMem src){
-    dest.pc = src.pc;
-    dest.instruction = src.instruction;
-
-    dest.result = src.result;    // result of the EX stage
-    dest.rd = src.rd;        // destination register
-    dest.useRd = src.useRd;
-    dest.isLongInstruction = src.isLongInstruction;
-    dest.opCode = src.opCode;    // LD or ST (can be reduced to 2 bits)
-    dest.funct3 = src.funct3;    // datasize and sign extension bit
-
-    dest.datac = src.datac;     // data to be stored in memory or csr result
-
-    //For branch unit
-    dest.nextPC = src.nextPC;
-    dest.isBranch = src.isBranch;
-
-    //Register for all stages
-    dest.we = src.we;
-}
-
-void copyMemtoWB(struct MemtoWB &dest, struct MemtoWB src){
-    dest.result = src.result;    // Result to be written back
-    dest.rd = src.rd;        // destination register
-    dest.useRd = src.useRd;
-
-    dest.address = src.address;
-    dest.valueToWrite = src.valueToWrite;
-    dest.byteEnable = src.byteEnable;
-    dest.isStore = src.isStore;
-    dest.isLoad = src.isLoad;
-
-    //Register for all stages
-    dest.we = src.we;
-}
-*/
 
 
-
-
-
-void doCycle(struct Core &core, 		 //Core containing all values
-		bool globalStall)
+void doCycle(struct Core &core, bool globalStall)
 {
-	bool localStall = globalStall;
+	//declare temporary structs
+  struct FtoDC ftoDC_temp; ftoDC_temp.pc = 0; ftoDC_temp.instruction = 0; ftoDC_temp.nextPCFetch = 0; ftoDC_temp.we = 0;
+  struct DCtoEx dctoEx_temp; dctoEx_temp.isBranch = 0; dctoEx_temp.useRs1 = 0; dctoEx_temp.useRs2 = 0; dctoEx_temp.useRs3 = 0; dctoEx_temp.useRd = 0; dctoEx_temp.we = 0;
+  struct ExtoMem extoMem_temp; extoMem_temp.useRd = 0; extoMem_temp.isBranch = 0; extoMem_temp.we = 0;
+  struct MemtoWB memtoWB_temp; memtoWB_temp.useRd = 0; memtoWB_temp.isStore = 0; memtoWB_temp.we = 0; memtoWB_temp.isLoad = 0;
+  struct WBOut wbOut_temp; wbOut_temp.useRd = 0; wbOut_temp.we = 0; wbOut_temp.rd = 0;
+  struct ForwardReg forwardRegisters; forwardRegisters.forwardExtoVal1 = 0; forwardRegisters.forwardExtoVal2 = 0; forwardRegisters.forwardExtoVal3 = 0; forwardRegisters.forwardMemtoVal1 = 0; forwardRegisters.forwardMemtoVal2 = 0; forwardRegisters.forwardMemtoVal3 = 0; forwardRegisters.forwardWBtoVal1 = 0; forwardRegisters.forwardWBtoVal2 = 0; forwardRegisters.forwardWBtoVal3 = 0;
 
-    core.stallSignals[0] = 0; core.stallSignals[1] = 0; core.stallSignals[2] = 0; core.stallSignals[3] = 0; core.stallSignals[4] = 0;
-    core.stallIm = false; core.stallDm = false; core.stallAlu = false;
-    bool localStallAlu = false;
-
-    //declare temporary structs
-    struct FtoDC ftoDC_temp; ftoDC_temp.pc = 0; ftoDC_temp.instruction = 0; ftoDC_temp.nextPCFetch = 0; ftoDC_temp.we = 0;
-    struct DCtoEx dctoEx_temp; dctoEx_temp.isBranch = 0; dctoEx_temp.useRs1 = 0; dctoEx_temp.useRs2 = 0; dctoEx_temp.useRs3 = 0; dctoEx_temp.useRd = 0; dctoEx_temp.we = 0;
-    struct ExtoMem extoMem_temp; extoMem_temp.useRd = 0; extoMem_temp.isBranch = 0; extoMem_temp.we = 0;
-    struct MemtoWB memtoWB_temp; memtoWB_temp.useRd = 0; memtoWB_temp.isStore = 0; memtoWB_temp.we = 0; memtoWB_temp.isLoad = 0;
-    struct WBOut wbOut_temp; wbOut_temp.useRd = 0; wbOut_temp.we = 0; wbOut_temp.rd = 0;
-    struct ForwardReg forwardRegisters; forwardRegisters.forwardExtoVal1 = 0; forwardRegisters.forwardExtoVal2 = 0; forwardRegisters.forwardExtoVal3 = 0; forwardRegisters.forwardMemtoVal1 = 0; forwardRegisters.forwardMemtoVal2 = 0; forwardRegisters.forwardMemtoVal3 = 0; forwardRegisters.forwardWBtoVal1 = 0; forwardRegisters.forwardWBtoVal2 = 0; forwardRegisters.forwardWBtoVal3 = 0;
-
-
-
-    //declare temporary register file
-    ac_int<32, false> nextInst;
-
-    if (!localStall && !core.stallDm)
-    	core.im->process(core.pc, WORD, LOAD, 0, nextInst, core.stallIm);
-
-    fetch(core.pc, ftoDC_temp, nextInst);
-    decode(core.ftoDC, dctoEx_temp, core.regFile);
-    core.basicALU.process(core.dctoEx, extoMem_temp, core.stallAlu);	//calling ALU: execute stage
+  //run all pipeline stages
+  ftoDC_temp = fetch(core.pc)
+  // fetch(ac_int<32, false> pc, static IncompleteMemory imInterface)
+  //run control unit
+}
 
 
-    memory(core.extoMem, memtoWB_temp);
-    writeback(core.memtoWB, wbOut_temp);
-
-    //We update localStall value according to stallAlu
-    localStall |= core.stallAlu;
-
-    //resolve stalls, forwards
-    if (!localStall)
-		forwardUnit(dctoEx_temp.rs1, dctoEx_temp.useRs1,
-				dctoEx_temp.rs2, dctoEx_temp.useRs2,
-				dctoEx_temp.rs3, dctoEx_temp.useRs3,
-				extoMem_temp.rd, extoMem_temp.useRd, extoMem_temp.isLongInstruction,
-				memtoWB_temp.rd, memtoWB_temp.useRd,
-				wbOut_temp.rd, wbOut_temp.useRd,
-				core.stallSignals, forwardRegisters);
-
-    if (!core.stallSignals[STALL_MEMORY] && !localStall && memtoWB_temp.we && !core.stallIm){
-
-       memMask mask;
-       //TODO: carry the data size to memToWb
-       switch (core.extoMem.funct3) {
-         case 0:
-          mask = BYTE;
-          break;
-         case 1:
-          mask = HALF;
-          break;
-        case 2:
-          mask = WORD;
-          break;
-        case 4:
-          mask = BYTE_U;
-          break;
-        case 5:
-          mask = HALF_U;
-          break;
-        //Should NEVER happen
-        default:
-          mask = WORD;
-          break;
-       }
-       core.dm->process(memtoWB_temp.address, mask, memtoWB_temp.isLoad ? LOAD : (memtoWB_temp.isStore ? STORE : NONE), memtoWB_temp.valueToWrite, memtoWB_temp.result, core.stallDm);
-    }
-    //commit the changes to the pipeline register
-    if (!core.stallSignals[STALL_FETCH] && !localStall && !core.stallIm && !core.stallDm){
-        core.ftoDC = ftoDC_temp;
-    }
-
-    if (!core.stallSignals[STALL_DECODE] && !localStall && !core.stallIm && !core.stallDm){
-        core.dctoEx = dctoEx_temp;
-
-    	if (forwardRegisters.forwardExtoVal1 && extoMem_temp.we)
-      	  core.dctoEx.lhs = extoMem_temp.result;
-    	else if (forwardRegisters.forwardMemtoVal1 && memtoWB_temp.we)
-    		core.dctoEx.lhs = memtoWB_temp.result;
-    	else if (forwardRegisters.forwardWBtoVal1 && wbOut_temp.we)
-    		core.dctoEx.lhs = wbOut_temp.value;
-
-    	if (forwardRegisters.forwardExtoVal2 && extoMem_temp.we)
-    		core.dctoEx.rhs = extoMem_temp.result;
-    	else if (forwardRegisters.forwardMemtoVal2 && memtoWB_temp.we)
-    		core.dctoEx.rhs = memtoWB_temp.result;
-    	else if (forwardRegisters.forwardWBtoVal2 && wbOut_temp.we)
-    		core.dctoEx.rhs = wbOut_temp.value;
-
-    	if (forwardRegisters.forwardExtoVal3 && extoMem_temp.we)
-    		core.dctoEx.datac = extoMem_temp.result;
-    	else if (forwardRegisters.forwardMemtoVal3 && memtoWB_temp.we)
-    		core.dctoEx.datac = memtoWB_temp.result;
-    	else if (forwardRegisters.forwardWBtoVal3 && wbOut_temp.we)
-    		core.dctoEx.datac = wbOut_temp.value;
-    }
-
-    if (core.stallSignals[STALL_DECODE] && !core.stallSignals[STALL_EXECUTE] && !core.stallIm && !core.stallDm && !localStall){
-    	core.dctoEx.we = 0; core.dctoEx.useRd = 0; core.dctoEx.isBranch = 0; core.dctoEx.instruction = 0; core.dctoEx.pc = 0;
-    }
-
-    if (!core.stallSignals[STALL_EXECUTE] && !localStall && !core.stallIm && !core.stallDm){
-        core.extoMem = extoMem_temp;
-    }
-
-    if (!core.stallSignals[STALL_MEMORY] && !localStall && !core.stallIm && !core.stallDm){
-        core.memtoWB = memtoWB_temp;
-    }
-
-    if (wbOut_temp.we && wbOut_temp.useRd && !localStall && !core.stallIm && !core.stallDm){
-    	core.regFile[wbOut_temp.rd] = wbOut_temp.value;
-    	core.cycle++;
-    }
+void doControl(bool globalStall,
+     struct FtoDC ftoDC_temp, struct FtoDC &ftoDC,
+     struct DCtoEx dctoEx_temp, struct DCtoEx &dctoEx,
+     struct ExtoMem extoMem_temp, struct ExtoMem &extoMem,
+     struct MemtoWB memtoWB_temp, struct MemtoWB &memtoWB, bool
+&wasLoad_out,
+     bool wasLoad, struct WBOut &wbOut,
+     ac_int<32, false> pc_in, ac_int<32, false> &pc_out,
+     ac_int<32, false> imData[DRAM_SIZE>>2], ac_int<32, false>
+dmData[DRAM_SIZE>>2],
+     ac_int<32, true> regFile[32])
+{
 
 
-	branchUnit(ftoDC_temp.nextPCFetch, dctoEx_temp.nextPCDC, dctoEx_temp.isBranch, extoMem_temp.nextPC, extoMem_temp.isBranch, core.pc, core.ftoDC.we, core.dctoEx.we, core.stallSignals[STALL_FETCH] || core.stallIm || core.stallDm || localStall);
+     static IncompleteMemory imInterface = IncompleteMemory(imData);
+     static IncompleteMemory dmInterface = IncompleteMemory(dmData);
+
+     struct ForwardReg forwardRegisters;
+     struct WBOut wbOut_temp;
+
+     if (wasLoad)
+         wbOut.value = valueRead;
+
+     extoMem.funct3 = 0;
+
+     bool localStall = globalStall;
+     bool stallSignals[5];
+
+     stallSignals[0] = 0; stallSignals[1] = 0; stallSignals[2] = 0;
+stallSignals[3] = 0; stallSignals[4] = 0;
+     bool stallIm = false;
+     bool stallDm = false;
+     bool stallAlu = false;
+     bool localStallAlu = false;
+
+     //We update localStall value according to stallAlu
+     localStall |= stallAlu;
+
+     //resolve stalls, forwards
+     if (!localStall)
+         forwardUnit(dctoEx_temp.rs1, dctoEx_temp.useRs1,
+                 dctoEx_temp.rs2, dctoEx_temp.useRs2,
+                 dctoEx_temp.rs3, dctoEx_temp.useRs3,
+                 extoMem_temp.rd, extoMem_temp.useRd,
+extoMem_temp.isLongInstruction,
+                 memtoWB_temp.rd, memtoWB_temp.useRd,
+                 wbOut_temp.rd, wbOut_temp.useRd,
+                 stallSignals, forwardRegisters);
+
+     if (!stallSignals[STALL_MEMORY] && !localStall && memtoWB_temp.we
+&& !stallIm){
+
+        memMask mask;
+        //TODO: carry the data size to memToWb
+        switch (extoMem.funct3) {
+          case 0:
+           mask = BYTE;
+           break;
+          case 1:
+           mask = HALF;
+           break;
+         case 2:
+           mask = WORD;
+           break;
+         case 4:
+           mask = BYTE_U;
+           break;
+         case 5:
+           mask = HALF_U;
+           break;
+         //Should NEVER happen
+         default:
+           mask = WORD;
+           break;
+        }
+        wasLoad_out = memtoWB_temp.isLoad;
+        dmInterface.process(memtoWB_temp.address, mask,
+memtoWB_temp.isLoad ? LOAD : (memtoWB_temp.isStore ? STORE : NONE),
+memtoWB_temp.valueToWrite, valueRead, stallDm);
+     }
+     //commit the changes to the pipeline register
+     if (!stallSignals[STALL_FETCH] && !localStall && !stallIm && !stallDm){
+         ftoDC = ftoDC_temp;
+     }
+
+     if (!stallSignals[STALL_DECODE] && !localStall && !stallIm &&
+!stallDm){
+         dctoEx = dctoEx_temp;
+
+         if (forwardRegisters.forwardExtoVal1 && extoMem_temp.we)
+             dctoEx.lhs = extoMem_temp.result;
+         else if (forwardRegisters.forwardMemtoVal1 && memtoWB_temp.we)
+             dctoEx.lhs = memtoWB_temp.result;
+         else if (forwardRegisters.forwardWBtoVal1 && wbOut_temp.we)
+             dctoEx.lhs = wbOut_temp.value;
+
+         if (forwardRegisters.forwardExtoVal2 && extoMem_temp.we)
+             dctoEx.rhs = extoMem_temp.result;
+         else if (forwardRegisters.forwardMemtoVal2 && memtoWB_temp.we)
+             dctoEx.rhs = memtoWB_temp.result;
+         else if (forwardRegisters.forwardWBtoVal2 && wbOut_temp.we)
+             dctoEx.rhs = wbOut_temp.value;
+
+         if (forwardRegisters.forwardExtoVal3 && extoMem_temp.we)
+             dctoEx.datac = extoMem_temp.result;
+         else if (forwardRegisters.forwardMemtoVal3 && memtoWB_temp.we)
+             dctoEx.datac = memtoWB_temp.result;
+         else if (forwardRegisters.forwardWBtoVal3 && wbOut_temp.we)
+             dctoEx.datac = wbOut_temp.value;
+     }
+
+     if (stallSignals[STALL_DECODE] && !stallSignals[STALL_EXECUTE] &&
+!stallIm && !stallDm && !localStall){
+         dctoEx.we = 0; dctoEx.useRd = 0; dctoEx.isBranch = 0;
+dctoEx.instruction = 0; dctoEx.pc = 0;
+     }
+
+     if (!stallSignals[STALL_EXECUTE] && !localStall && !stallIm &&
+!stallDm){
+         extoMem = extoMem_temp;
+     }
+
+     if (!stallSignals[STALL_MEMORY] && !localStall && !stallIm &&
+!stallDm){
+         memtoWB = memtoWB_temp;
+     }
+
+     if (wbOut_temp.we && wbOut_temp.useRd && !localStall && !stallIm &&
+!stallDm){
+         regFile[wbOut_temp.rd] = wbOut_temp.value;
+     }
+
+
+     branchUnit(ftoDC_temp.nextPCFetch, dctoEx_temp.nextPCDC,
+dctoEx_temp.isBranch, extoMem_temp.nextPC, extoMem_temp.isBranch,
+pc_out, ftoDC.we, dctoEx.we, stallSignals[STALL_FETCH] || stallIm ||
+stallDm || localStall);
 
 }
+
 
 //void doCore(IncompleteMemory im, IncompleteMemory dm, bool globalStall)
 void doCore(bool globalStall, ac_int<32, false> imData[DRAM_SIZE>>2], ac_int<32, false> dmData[DRAM_SIZE>>2])
