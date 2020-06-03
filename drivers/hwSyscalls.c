42f86e91fc9463788724cbcef32ec7f296cc0fbe
srokicki <simon.rokicki@irisa.fr>
2020-06-03 10:27:35 +0200

    Change adress of statusReg

--- drivers/hwSyscalls.c
+++ drivers/hwSyscalls.c
@@ -1,30 +1,28 @@
 #include <machine/syscall.h>
 #include <sys/types.h>
 
-#define STATUSREG_ADDRESS 0x300001
+#define STATUSREG_ADDRESS 0x300004
 #define STDOUTREG_ADDRESS 0x300000
 
 #define EXITFLAG 0
 
 /* Write to a file.  */
-ssize_t
-_write(int file, const void *ptr, size_t len)
+ssize_t _write(int file, const void* ptr, size_t len)
 {
 
-    char* serial = (char*) STDOUTREG_ADDRESS;
-    for (unsigned int oneChar=0; oneChar<len; oneChar++)
-        *serial = ((char*)ptr)[oneChar];
+  int* serial = (int*)STDOUTREG_ADDRESS;
+  for (unsigned int oneChar = 0; oneChar < len; oneChar++)
+    *serial = ((char*)ptr)[oneChar];
 
   return len;
 }
 
 /* Exit a program without cleaning up files.  */
-void
-_exit(int exit_status)
+void _exit(int exit_status)
 {
-  char* statusRegister = (char*) STATUSREG_ADDRESS;
-  //set the exit flag
+  int* statusRegister = (int*)STATUSREG_ADDRESS;
+  // set the exit flag
   *statusRegister |= (1 << EXITFLAG);
-  while (1);
+  while (1)
+    ;
 }
-
