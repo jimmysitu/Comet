4095f20a8a71aa61aacad2bfacefd8b3791f5fc6
srokicki <simon.rokicki@irisa.fr>
2020-06-19 16:24:11 +0200

    Add driver for sbrk (ie mallocs)

--- drivers/hwSyscalls.c
+++ drivers/hwSyscalls.c
@@ -26,3 +26,23 @@ void _exit(int exit_status)
   while (1)
     ;
 }
+
+extern char _end[]; /* _end is set in the linker command file */
+char* heap_ptr;
+
+/*
+ * sbrk -- changes heap size size. Get nbytes more
+ *         RAM. We just increment a pointer in what's
+ *         left of memory on the board.
+ */
+char* _sbrk(nbytes) int nbytes;
+{
+  char* base;
+
+  if (!heap_ptr)
+    heap_ptr = (char*)&_end;
+  base = heap_ptr;
+  heap_ptr += nbytes;
+
+  return base;
+}
