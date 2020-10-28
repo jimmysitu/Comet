c599b651d1ca1cc83ca2bf978a0acca82abb0079
Jimmy Situ <web@jimmystone.cn>
2020-10-28 08:58:42 +0800

    Add artix7 device

--- scripts/vivado_artix7.tcl
+++ scripts/vivado_artix7.tcl
@@ -1,14 +1,14 @@
 # Create a Vivado HLS project
 set WORKING_DIR $::env(PWD)
 
-open_project -reset Comet
+open_project -reset Comet_artix7
 set_top doCore
 add_files $WORKING_DIR/../src/core.cpp \
     -cflags "-I$WORKING_DIR/../include -D__VIVADO__ -D__HLS__ -DMEMORY_INTERFACE=IncompleteMemory -std=c++11 -Wall"
 
 # Solution *************************
 open_solution -reset "default"
-set_part {xc7z020clg400-2}
+set_part {xc7a12tcsg325-2}
 create_clock -period 10 -name default
 
 # Directives *************************
@@ -20,8 +20,10 @@ set_directive_resource -core RAM_1P "doCore" imData
 set_directive_interface -mode bram "doCore" dmData
 set_directive_resource -core RAM_1P "doCore" dmData
 
-#FIXME:set_directive_resource -core RAM_1P auto "doCore" core.regFile
+set_directive_resource -core XPM_MEMORY -memory_style auto "doCore" core.regFile
+
 set_directive_pipeline -II 1 "doCycle"
+set_directive_latency -min 4 -max 6 "doCycle"
 
 # Run C simulation
 #csim_design
@@ -31,7 +33,8 @@ csynth_design
 #cosim_design
 # Create the IP package
 export_design -flow syn -format ip_catalog -rtl verilog \
-    -vendor "www.jimmystone.cn" -version "0.1.0"
+    -library "hsl" \
+    -vendor "jimmystone.cn" -version "0.1.0"
 
 exit
 
---
+++ scripts/vivado_zynq7.tcl
@@ -0,0 +1,38 @@
+# Create a Vivado HLS project
+set WORKING_DIR $::env(PWD)
+
+open_project -reset Comet_zynq7
+set_top doCore
+add_files $WORKING_DIR/../src/core.cpp \
+    -cflags "-I$WORKING_DIR/../include -D__VIVADO__ -D__HLS__ -DMEMORY_INTERFACE=IncompleteMemory -std=c++11 -Wall"
+
+# Solution *************************
+open_solution -reset "default"
+set_part {xc7z020clg400-2}
+create_clock -period 10 -name default
+
+# Directives *************************
+set_directive_interface -mode ap_none "doCore" globalStall
+set_directive_interface -mode bram "doCore" imData
+set_directive_interface -mode bram "doCore" dmData
+
+set_directive_resource -core XPM_MEMORY -memory_style auto "doCore" core.regFile
+
+set_directive_pipeline -II 1 "doCycle"
+set_directive_latency -min 4 -max 6 "doCycle"
+
+# Run C simulation
+#csim_design
+# Run Synthesis
+csynth_design
+# Run RTL verification
+#cosim_design
+# Create the IP package
+export_design -flow syn -format ip_catalog -rtl verilog \
+    -library "hsl" \
+    -vendor "jimmystone.cn" -version "0.1.0"
+
+exit
+
+
+
