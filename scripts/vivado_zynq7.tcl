02310601d947e1caab99c09b540b3f5fb00fe514
Jimmy Situ <web@jimmystone.cn>
2020-10-31 14:44:03 +0800

    Add implement for AXI interface

--- scripts/vivado_zynq7.tcl
+++ scripts/vivado_zynq7.tcl
@@ -36,7 +36,40 @@ csynth_design
 export_design -format ip_catalog -rtl verilog \
     -library "hsl" \
     -vendor "jimmystone.cn" -version "0.1.0" \
-    -flow syn
+
+#    -flow syn
+
+open_solution -reset "axi_interface"
+set_part {xc7z020clg400-2}
+create_clock -period 10 -name default
+config_bind -effort high
+config_schedule -effort high -verbose
+
+# Directives *************************
+set_directive_interface -mode ap_none "doCore" globalStall
+set_directive_interface -mode m_axi -latency 1 "doCore" imData
+set_directive_interface -mode m_axi -latency 1 "doCore" dmData
+
+set_directive_resource -latency 1 "doCore" core.regFile
+set_directive_dependence -variable core.regFile -direction WAR -type intra -dependent false "doCycle"
+set_directive_dependence -variable core.dctoEx.lhs -direction WAR -type intra -dependent false "doCycle"
+set_directive_dependence -variable core.dctoEx.rhs -direction WAR -type intra -dependent false "doCycle"
+set_directive_dependence -variable core.dctoEx.datac -direction WAR -type intra -dependent false "doCycle"
+
+set_directive_pipeline -II 1 -rewind "doCycle"
+
+# Run C simulation
+#csim_design
+# Run Synthesis
+csynth_design
+# Run RTL verification
+#cosim_design
+# Create the IP package
+export_design -format ip_catalog -rtl verilog \
+    -library "hsl" \
+    -vendor "jimmystone.cn" -version "0.1.0" \
+
+#    -flow syn
 
 exit
 
