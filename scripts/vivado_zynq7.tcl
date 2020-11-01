68c570b9fd3da78924de0733bd5e43ac0cef196b
Jimmy Situ <web@jimmystone.cn>
2020-11-01 22:12:21 +0800

    Add initCore, fix directive bug for II

--- scripts/vivado_zynq7.tcl
+++ scripts/vivado_zynq7.tcl
@@ -6,7 +6,7 @@ set_top doCore
 add_files $WORKING_DIR/../src/core.cpp \
     -cflags "-I$WORKING_DIR/../include -D__VIVADO__ -D__HLS__ -DMEMORY_INTERFACE=IncompleteMemory -std=c++11 -Wall"
 
-# Solution *************************
+# Solution for SRAM interface *********************
 open_solution -reset "default"
 set_part {xc7z020clg400-2}
 create_clock -period 10 -name default
@@ -18,45 +18,16 @@ set_directive_interface -mode ap_none "doCore" globalStall
 set_directive_interface -mode ap_memory -latency 1 "doCore" imData
 set_directive_interface -mode ap_memory -latency 1 "doCore" dmData
 
-set_directive_resource -latency 1 "doCore" core.regFile
-set_directive_dependence -variable core.regFile -direction WAR -type intra -dependent false "doCycle"
-set_directive_dependence -variable core.dctoEx.lhs -direction WAR -type intra -dependent false "doCycle"
-set_directive_dependence -variable core.dctoEx.rhs -direction WAR -type intra -dependent false "doCycle"
-set_directive_dependence -variable core.dctoEx.datac -direction WAR -type intra -dependent false "doCycle"
+# To achieve 1W2R register file, directive core.regFile to registers
+set_directive_array_partition -dim 0 "doCore" core.regFile
+set_directive_pipeline -II 1 "doCycle"
 
-set_directive_pipeline -II 1 -rewind "doCycle"
-
-# Run C simulation
-#csim_design
-# Run Synthesis
-csynth_design
-# Run RTL verification
-#cosim_design
-# Create the IP package
-export_design -format ip_catalog -rtl verilog \
-    -library "hsl" \
-    -vendor "jimmystone.cn" -version "0.1.0" \
-
-#    -flow syn
-
-open_solution -reset "axi_interface"
-set_part {xc7z020clg400-2}
-create_clock -period 10 -name default
-config_bind -effort high
-config_schedule -effort high -verbose
-
-# Directives *************************
-set_directive_interface -mode ap_none "doCore" globalStall
-set_directive_interface -mode m_axi -latency 1 "doCore" imData
-set_directive_interface -mode m_axi -latency 1 "doCore" dmData
-
-set_directive_resource -latency 1 "doCore" core.regFile
-set_directive_dependence -variable core.regFile -direction WAR -type intra -dependent false "doCycle"
-set_directive_dependence -variable core.dctoEx.lhs -direction WAR -type intra -dependent false "doCycle"
-set_directive_dependence -variable core.dctoEx.rhs -direction WAR -type intra -dependent false "doCycle"
-set_directive_dependence -variable core.dctoEx.datac -direction WAR -type intra -dependent false "doCycle"
-
-set_directive_pipeline -II 1 -rewind "doCycle"
+#FIXME: Try to pipeline manually, not work
+#set_directive_latency -min 1 "fetch"
+#set_directive_latency -min 1 "decode"
+#set_directive_latency -min 1 "execute"
+#set_directive_latency -min 1 "memory"
+#set_directive_latency -min 1 "writeback"
 
 # Run C simulation
 #csim_design
@@ -68,8 +39,36 @@ csynth_design
 export_design -format ip_catalog -rtl verilog \
     -library "hsl" \
     -vendor "jimmystone.cn" -version "0.1.0" \
+    -flow impl
 
-#    -flow syn
+## Solution for AXI interface *********************
+#open_solution -reset "axi_interface"
+#set_part {xc7z020clg400-2}
+#create_clock -period 10 -name default
+#config_bind -effort high
+#config_schedule -effort high -verbose
+#
+## Directives *************************
+#set_directive_interface -mode ap_none "doCore" globalStall
+#set_directive_interface -mode m_axi -latency 1 "doCore" imData
+#set_directive_interface -mode m_axi -latency 1 "doCore" dmData
+#
+#
+#set_directive_array_partition -dim 0 "doCore" core.regFile
+#set_directive_pipeline -II 1 "doCycle"
+#
+## Run C simulation
+##csim_design
+## Run Synthesis
+#csynth_design
+## Run RTL verification
+##cosim_design
+## Create the IP package
+#export_design -format ip_catalog -rtl verilog \
+#    -library "hsl" \
+#    -vendor "jimmystone.cn" -version "0.1.0" \
+#
+##    -flow syn
 
 exit
 
